ec3129cdbcc9b4a289ee2ea0adc33b71a0159e52
Osama Lababidi <osama.lababidi@etu.univ-grenoble-alpes.fr>
2024-03-20 15:12:46 +0100

    ajoute de la fonctionnalité permettant de faire une recherche dynamique

--- js/controller/RecipeController.js
+++ js/controller/RecipeController.js
@@ -3,29 +3,44 @@ class RecipeController {
       this.model = model;
       this.view = view;
       this.setupEventListeners();
-      this.view.renderFavorites(this.model.getFavoritesFromSession()); // Initialiser l'affichage des favoris
+      this.rechercheDynamiqueActive = true; // Par défaut, la recherche dynamique est activée
+      this.view.renderFavorites(this.model.getFavoritesFromSession());
     }
     
     setupEventListeners() {
       const searchBtn = document.getElementById('btn-lancer-recherche');
+      // Le bouton de recherche peut rester pour une recherche explicite si nécessaire,
+      // mais n'est pas utilisé pour la recherche dynamique
       searchBtn.addEventListener('click', () => this.onSearch());
-  
+
       const favBtn = document.getElementById('btn-favoris');
       favBtn.addEventListener('click', () => this.addFavorite());
-  
+
+      const toggleRechercheDynamique = document.getElementById('toggle-recherche-dynamique');
+      toggleRechercheDynamique.addEventListener('change', () => {
+        this.rechercheDynamiqueActive = toggleRechercheDynamique.checked;
+      });
+
       const inputSearch = document.getElementById('input-recherche');
       inputSearch.addEventListener('input', () => {
         const query = this.view.getInputValue();
         this.toggleFavoriteButtonState(query);
         this.view.toggleFavoriteStar(this.model.isFavorite(query));
+         if (this.rechercheDynamiqueActive && inputSearch.value.length >= 3) {
+        this.onSearch();
+        } else {
+          this.view.clearResults(); // Efface les résultats si la requête est trop courte
+        }
       });
     }
   
     onSearch() {
       const query = this.view.getInputValue();
-      this.model.fetchRecipes(query, recipes => {
-        this.view.renderResults(recipes);
-      });
+      if (query.length >= 3) { // S'assure que la requête est suffisamment longue
+        this.model.fetchRecipes(query, recipes => {
+          this.view.renderResults(recipes);
+        });
+      }
     }
   
     addFavorite() {
@@ -38,7 +53,6 @@ class RecipeController {
   
     toggleFavoriteButtonState(query) {
       const favBtn = document.getElementById('btn-favoris');
-      favBtn.disabled = query.length < 3; // Désactiver le bouton si la requête est de moins de 3 caractères
+      favBtn.disabled = query.length < 3;
     }
-  }
-  
+}
