455d1db05ec8bded479feb68b3fed57c3a9a25e4
Osama Lababidi <osama.lababidi@etu.univ-grenoble-alpes.fr>
2024-03-24 20:44:53 +0100

    Documentation de recipeView et changement de nom de variable

--- js/view/RecipeView.js
+++ js/view/RecipeView.js
@@ -1,44 +1,52 @@
 class RecipeView {
   constructor(model) {
     this.model = model;
-    this.inputRecherche = document.getElementById("search-input");
-    this.blocResultats = document.getElementById("bloc-resultats");
-    this.listeFavoris = document.getElementById("liste-favorite");
-    this.infoVide = document.getElementById("info-vide"); // Make sure this also has an ID in your HTML.
-    this.initRecipePanel();
-    this.renderFavorites();
+    // Initialization of DOM elements by ID, ensuring elements exist in the HTML.
+    this.searchInput = document.getElementById("search-input");
+    this.blocResults = document.getElementById("block-results");
+    this.favList = document.getElementById("list-favorite");
+    // Initial setup methods to prepare the UI on load.
+    this.initRecipePanel(); // Prepares the recipe panel for displaying saved recipes.
+    this.renderFavorites(); // Populates the favorites list based on stored session data.
   }
 
+  //Retrieves the current value from the search input field.
   getInputValue() {
-    return this.inputRecherche.value;
+    return this.searchInput.value;
   }
 
+  //Clears the search results display area, preparing it for new results.
   clearResults() {
     this.blocResultats.innerHTML = "";
   }
 
+  //Displays search results. Each result can be clicked to show more details.
   renderResults(recipes) {
-    this.clearResults();
+    this.clearResults(); //Ensures the area is clear before adding new results.
     recipes.forEach((recipe) => {
       const paragraphe = document.createElement("p");
       paragraphe.textContent = recipe.recipe.label;
       this.blocResultats.appendChild(paragraphe);
+      // Adds click listener to each result for displaying detailed popup.
       paragraphe.addEventListener("click", () =>
         this.showRecipePopup(recipe.recipe)
       );
     });
   }
 
+  //Generates the HTML structure for a recipe popup, including save (bookmark) functionality.
   showRecipePopup(recipe) {
     const popup = this.createPopup(recipe);
     this.setupSaveRecipeListener(popup, recipe);
     this.setupClosePopupListener(popup);
     this.loadYouTubeVideo(popup, recipe);
-    document.body.appendChild(popup);
+    document.body.appendChild(popup); // Adds the popup to the document.
   }
 
+  //Generates the HTML structure for a recipe popup, including save (bookmark) functionality.
   createPopup(recipe) {
     const isSaved = this.model.isRecipeSaved(recipe.uri);
+    //Detailed popup creation with conditional content based on the recipe's saved status.
     const popup = document.createElement("div");
     popup.id = "recipe-popup";
     popup.innerHTML = `
@@ -79,24 +87,27 @@ class RecipeView {
     `;
     return popup;
   }
-
+ //Sets up an event listener for saving or unsaving a recipe. It toggles the visual state of the bookmark icon and updates the model accordingly.
   setupSaveRecipeListener(popup, recipe) {
     const checkbox = popup.querySelector("input[type=checkbox]");
     const bookmarkIcon = popup.querySelector(".bookmark-icon");
     checkbox.addEventListener("change", () => {
       this.model.toggleRecipeInSession(recipe);
+      // Visually updates the bookmark icon based on the checkbox's checked state.
       bookmarkIcon.style.fill = checkbox.checked ? "white" : "none";
       this.updateRecipePanel(); // Assurez-vous que cette ligne est présente
     });
   }
 
+  //Attaches an event listener to the popup's close button, allowing users to close the popup.
   setupClosePopupListener(popup) {
     const closeButton = popup.querySelector("#close-popup");
     closeButton.addEventListener("click", () => {
-      popup.remove();
+      popup.remove(); // Removes the popup from the DOM when the close button is clicked.
     });
   }
 
+  //Asynchronously fetches and displays a YouTube video related to the recipe within the popup.
   loadYouTubeVideo(popup, recipe) {
     this.model
       .fetchYouTubeVideo(recipe.label)
@@ -111,6 +122,7 @@ class RecipeView {
       .catch((error) => console.error("Error loading YouTube video:", error));
   }
 
+  //Adds a search query to the favorites list if it meets the length requirement. This method integrates with the model to store the favorite and updates the UI accordingly.
   addFavorite(query) {
     if (query.length < 3) return;
     this.model.addFavoriteToSession(query);
@@ -118,13 +130,17 @@ class RecipeView {
     this.toggleFavoriteStar(true);
   }
 
+  //Toggles the favorite star icon between filled and empty states based on the isFavorite boolean. This visual cue helps users identify if the current search or recipe is marked as a favorite.
   toggleFavoriteStar(isFavorite) {
-    const starImg = document.getElementById("favorites-button").querySelector("img");
+    const starImg = document
+      .getElementById("favorites-button")
+      .querySelector("img");
     starImg.src = isFavorite
       ? "images/etoile-pleine.svg"
       : "images/etoile-vide.svg";
   }
 
+  //Checks if the current search query is a favorite and updates the favorite button's appearance and functionality based on this status.
   updateFavoriteStatus() {
     const query = this.getInputValue();
     const isFavorite = this.model.isFavorite(query);
@@ -133,56 +149,53 @@ class RecipeView {
     favBtn.disabled = query.length < 3;
   }
 
+  //Dynamically populates the favorites list with items from session storage. Each favorite can be clicked to perform a new search.
   renderFavorites() {
     const favorites = this.model.getFavoritesFromSession();
-    this.listeFavoris.innerHTML = "";
+    this.favList.innerHTML = "";
     favorites.forEach((fav) => {
       const li = document.createElement("li");
       li.textContent = fav;
-      this.listeFavoris.appendChild(li);
+      this.favList.appendChild(li);
       li.addEventListener("click", () => {
-        this.inputRecherche.value = fav;
-        this.controller.onSearch();
-        this.updateFavoriteStatus();
+        this.searchInput.value = fav; //Sets the search input to the selected favorite.
+        this.controller.onSearch(); //Triggers a search for the selected favorite.
+        this.updateFavoriteStatus(); // Updates the favorite status based on the new query.
       });
     });
     const currentQuery = this.getInputValue();
     this.toggleFavoriteStar(favorites.includes(currentQuery));
   }
 
+  //Toggles the display of the recipe panel, showing or hiding it based on its current state.
   toggleRecipePanel() {
     this.recipePanel.style.display =
       this.recipePanel.style.display === "none" ? "block" : "none";
-    this.updateRecipePanel();
+    this.updateRecipePanel(); // Updates the recipe panel content.
   }
 
+  //Refreshes the content of the recipe panel to show saved recipes from session storage. If no recipes are saved, it displays a message.
   updateRecipePanel() {
-    // Efface tout contenu précédent
-    this.recipePanel.innerHTML = "";
-
-    // Récupère les recettes sauvegardées depuis le modèle
+    this.recipePanel.innerHTML = ""; // Clears existing content.
     const savedRecipes = this.model.getSavedRecipesFromSession();
-
-    // Vérifie si des recettes sont sauvegardées, sinon affiche un message
     if (savedRecipes.length === 0) {
-        const noSavedRecipesMessage = document.createElement("p");
-        noSavedRecipesMessage.textContent = "Aucune recette sauvegardée.";
-        this.recipePanel.appendChild(noSavedRecipesMessage);
-        return; // Termine la méthode ici si pas de recettes
+      const noSavedRecipesMessage = document.createElement("p");
+      noSavedRecipesMessage.textContent = "No recipes saved.";
+      this.recipePanel.appendChild(noSavedRecipesMessage);
+      return;
     }
-
-    // Crée et ajoute des éléments pour chaque recette sauvegardée
     savedRecipes.forEach((recipe) => {
-        const recipeItem = document.createElement("p");
-        recipeItem.textContent = recipe.label;
-        recipeItem.style.cursor = "pointer";
-        recipeItem.addEventListener("click", () => this.showRecipePopup(recipe));
-        this.recipePanel.appendChild(recipeItem);
+      const recipeItem = document.createElement("p");
+      recipeItem.textContent = recipe.label;
+      this.recipePanel.appendChild(recipeItem);
+      recipeItem.addEventListener("click", () => this.showRecipePopup(recipe));
     });
-}
-
+  }
+  
+  //Initializes the recipe panel with predefined styles and appends it to the document body. This method sets up the panel for displaying saved recipes.
   initRecipePanel() {
     this.recipePanel = document.createElement("div");
+    // Sets various styling properties to ensure the panel is properly positioned and styled.
     this.recipePanel.id = "recipe-panel";
     this.recipePanel.style.position = "fixed";
     this.recipePanel.style.left = "0";
@@ -196,7 +209,8 @@ class RecipeView {
     document.body.appendChild(this.recipePanel);
   }
 
+  //Allows the view to communicate with its controller, facilitating MVC (Model-View-Controller) interaction patterns.
   setController(controller) {
-    this.controller = controller;
+    this.controller = controller; // Associates a controller with the view
   }
 }
